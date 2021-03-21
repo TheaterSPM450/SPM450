c96afc28d2990938d4bae96ab736aebbb20ab176
NickMacris <Nick.Macris@yahoo.com>
2021-03-20 21:01:24 -0400

    Pushed calibrate button down, added done button on calibrate page to return to start page

--- GUI.py
+++ GUI.py
@@ -10,7 +10,7 @@ calibratePage = Frame(root)
 for frame in (startPage, calibratePage):
     frame.grid(row=0, column=0, sticky='news')
 
-
+######################START PAGE###############################
 # initialize a horizontal slider
 sliderValue = IntVar()          # declaring an int in tkinter
 sliderValue.set(0)              # initializing it to zero
@@ -18,9 +18,17 @@ horiSlider = Scale(startPage, from_=0, to=100, orient=HORIZONTAL, variable=slide
 horiSlider.grid(row=0, column=0)
 
 calibrateButton = Button(startPage, text='Calibrate', command=calibratePage.tkraise)
-calibrateButton.grid(row=0, column=0)
+calibrateButton.grid(row=2, column=0)
+########################END START PAGE############################
 
-Label(calibratePage, text='Welcome to calibration').pack()
+#######################CALIBRATE PAGE#####################
+Label(calibratePage, text='Welcome to calibration').grid(row=0, column=0)
+doneButton = Button(calibratePage, text='Done', command=startPage.tkraise)
+doneButton.grid(row=1, column=0)
 
+
+
+
+#####################END CALIBRATE PAGE#########################
 startPage.tkraise()
 root.mainloop()
